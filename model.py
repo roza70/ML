bef1ab7b6929a4642a01e877b2ca2e2274525a89
tahsin-roza <tahsinroza10@gmail.com>
2026-04-03 11:25:51 +0600

    first commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.metrics import accuracy_score, classification_report
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import os
